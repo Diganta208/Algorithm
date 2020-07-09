44aa3c0b250b0631af01da81b7057007ac34207b
Diganta208 <57087564+Diganta208@users.noreply.github.com>
2020-07-09 08:15:15 -0700

    Add files via upload

---
+++ HeapImplemantation/Implement.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeapImplemantation
+{
+    class Implement
+    {
+
+        private int left(int i)
+        {
+            return i * 2;
+        }
+
+        private int right(int i)
+        {
+            return (i * 2 )+ 1;
+        }
+
+        private int parent(int i)
+        {
+            return i / 2;
+        }
+
+
+        public void heapSort(int[] heap, int heapSize)
+        {
+
+            int temp;
+            for(int i= heapSize; i>1; i--)
+            {
+                temp = heap[i];
+                heap[i] = heap[1];
+                heap[1] = temp;
+
+                heapSize--;
+                buildMaxheap(heap, heapSize, 1);
+            }
+
+        }
+
+        public  void buildMaxheap(int[] heap, int count, int parent)
+        {
+            int left = this.left(parent);
+            int right = this.right(parent);
+            int largest;
+
+            if(left<=count && heap[left]>heap[parent])
+            {
+                largest = left;
+
+            }
+            else
+            {
+                largest = parent;
+            }
+
+            if (right <= count && heap[right] > heap[largest])
+            {
+                largest = right;
+            }
+
+            if(largest!=parent )
+            {
+                int temp = heap[parent];
+                heap[parent] = heap[largest];
+                heap[largest] = temp;
+
+                buildMaxheap(heap,count, largest);
+            }
+
+
+        }
+    }
+}
---
+++ HeapImplemantation/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeapImplemantation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] heap = new int[10];
+
+            for (int i = 0; i < heap.Count(); i++)
+            {
+                heap[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Implement newImplement = new Implement();
+            for(int i=(heap.Count()-1)/2; i>=1; i--)
+            {
+                newImplement.buildMaxheap(heap,heap.Count(), i);
+            }
+            newImplement.heapSort(heap, heap.Count()-1);
+
+            for(int i = 0; i < heap.Count(); i++)
+            {
+                Console.WriteLine(heap[i]);
+            }
+
+        }
+    }
+}
