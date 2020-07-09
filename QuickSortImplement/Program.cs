f2df6ac4839b043fd80c2801d3aeaa138d6077ca
Diganta208 <57087564+Diganta208@users.noreply.github.com>
2020-07-09 08:47:15 -0700

    Add files via upload

---
+++ QuickSortImplement/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSortImplement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arr = new int[10];
+
+            for(int i=0; i<arr.Count(); i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            QuickSortCl aQuickSortCl = new QuickSortCl();
+            
+            aQuickSortCl.QuickSort(arr, 0, arr.Count() - 1);
+
+            for (int i = 0; i < arr.Count(); i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ QuickSortImplement/QuickSortCl.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSortImplement
+{
+    class QuickSortCl
+    {
+
+        public void QuickSort(int[] arr, int start, int end)
+        {
+            if(start>=end)
+            {
+                return;
+            }
+
+            int p = Partion(arr, start, end);
+            QuickSort(arr, start, p - 1);
+            QuickSort(arr, p + 1, end);
+
+               
+     
+        }
+
+
+        private int Partion(int[] arr, int start, int end)
+        {
+
+            int paivot = arr[end];
+            int i= start;
+            int temp;
+
+            for (int j = start; j <= end - 1; j++)
+            {
+                if(arr[j]<paivot)
+                {
+                   
+                    temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                    i++;
+                   
+
+                }
+            }
+
+            temp = paivot;
+            arr[end] = arr[i];
+            arr[i] = paivot;
+
+            return i;
+
+        }
+
+    }
+}
