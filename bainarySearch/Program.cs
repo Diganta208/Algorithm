0056d991f0d47142e14533664453b87a7c3ec14a
Diganta208 <57087564+Diganta208@users.noreply.github.com>
2020-07-09 08:13:18 -0700

    Add files via upload

---
+++ bainarySearch/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bainarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arr = new int[10];
+            for (int i = 0; i < arr.Count(); i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+
+            int V= Convert.ToInt32(Console.ReadLine());
+
+            Search aSearch = new Search();
+            int result= aSearch.bainarySearch(arr, V);
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+
+            
+        }
+    }
+}
---
+++ bainarySearch/Search.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bainarySearch
+{
+    class Search
+    {
+        public int bainarySearch (int[] arr, int V)
+        {
+            int left = 0;
+            int right = arr.Count() - 1;
+            int result = 0;
+
+            while (left <= right)
+            {
+                int mid = arr.Count() / 2;
+                if (arr[mid] == V)
+                {
+                    result = mid;
+                }
+                else if (V < arr[mid])
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+
+            return result;
+
+        }
+        
+       
+    }
+}
