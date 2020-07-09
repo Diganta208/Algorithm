ab2d74c815a51fb38e0ce097ce498c9ac52dc7c9
Diganta208 <57087564+Diganta208@users.noreply.github.com>
2020-07-09 08:22:49 -0700

    Add files via upload

---
+++ MargeSort/Marge.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MargeSort
+{
+    class Marge
+    {
+
+        public void MargeShort(int[] a, int left, int right)
+        {
+            if (right <= left)
+            { return;  }
+            int mid = (left + right) / 2;
+            MargeShort(a,left,mid);
+            MargeShort(a,mid+1,right);
+
+           finalMarge(a,left, right);
+
+        }
+
+         private void finalMarge(int[] a, int left, int right)
+        {
+            int mid = (left + right) / 2;
+            int i = left;
+            int j = mid+1;
+            int k = left;
+
+             int[] r = new int[100];
+             while (i <= mid && j <= right)
+             {
+                 if (a[i] < a[j])
+                 {
+                     r[k] = a[i];
+                     i++;
+                 }
+
+                 else
+                 {
+                     r[k] = a[j];
+                     j++;
+                 }
+                 k++;
+             }
+            while (i <= mid)
+             {
+                 r[k] = a[i];
+                 i++;
+                 k++;
+             }
+             while (j <= right)
+             {
+                 r[k] = a[j];
+                 j++;
+                 k++;
+             }
+
+             for(int m=left; m<=right; m++ )
+             {
+                 a[m] = r[m];
+             }
+
+             
+        }
+
+    }
+}
---
+++ MargeSort/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MargeSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] t = new int[10];
+
+            for (int i = 0; i < t.Count(); i++)
+            {
+                t[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Marge aMarge = new Marge();
+
+            
+            aMarge.MargeShort(t, 0, t.Count()-1);
+
+            for(int i=0; i<t.Count(); i++)
+            {
+                Console.WriteLine(t[i]);
+            }
+          
+
+            
+
+        }
+    }
+}
