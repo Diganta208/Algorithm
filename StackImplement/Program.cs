62816f27831b92b91e30fadfb3a3994168348cec
Diganta208 <57087564+Diganta208@users.noreply.github.com>
2020-07-09 08:48:55 -0700

    Add files via upload

---
+++ StackImplement/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackImplement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Stack aStack = new Stack();
+            aStack.push(7);
+            aStack.push(10);
+            aStack.pop();
+            aStack.push(13);
+        }
+    }
+}
---
+++ StackImplement/Stack.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackImplement
+{
+    class Stack
+    {
+
+        private int[] arr;
+
+        private  int top;
+        public Stack()
+        {
+            top = 0;
+            arr = new int[10];
+        }
+
+        public void push(int data)
+        {
+            if (top >= arr.Count())
+            {
+                Console.WriteLine("array is ful");
+            }
+
+            else
+            {
+                arr[top] = data;
+                top++;
+
+            }
+            
+        }
+
+        public int pop()
+        {
+            int item;
+            if(top==0)
+            {
+                Console.WriteLine("There is no element in this stack");
+                return -1;
+            }
+            else
+            {
+                top--;
+                item = arr[top];
+                return item;
+
+            }
+
+            
+        }
+    }
+}
