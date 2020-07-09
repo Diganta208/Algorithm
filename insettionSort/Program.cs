dfc8defc8d2f5beb6d104fa7df25d1a9ad691587
Diganta208 <57087564+Diganta208@users.noreply.github.com>
2020-07-09 08:16:48 -0700

    Add files via upload

---
+++ insettionSort/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace insettionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int[] arr = new int[10];
+
+            
+
+            for (int i = 1; i < arr.Count(); i++)
+            {
+                int item = arr[i];
+
+                int j = i - 1;
+
+                while (j >= 0 && arr[j] > item)
+                {
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+
+                    
+                }
+                arr[j + 1] = item;
+
+
+
+
+
+
+            }
+
+        }
+    }
+}
