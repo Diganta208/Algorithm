a71b1dff9b4622385668682cc6ca1cbf07eed2a8
Diganta208 <57087564+Diganta208@users.noreply.github.com>
2020-07-09 08:21:09 -0700

    Add files via upload

---
+++ LinkedList/Node.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class Node
+    {
+        public int data { get; set; }
+        public Node next { get; set; }
+
+        public Node(int data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+
+        public void print(Node head)
+        {
+            Node currentNode = head;
+            while(currentNode != null)
+            {
+                Console.WriteLine(currentNode.data);
+                currentNode = currentNode.next;
+            }
+        }
+
+
+        public void reverseLinkedList(Node head)
+        {
+            Node currentNode = head;
+            Node previous = null;
+
+            while(currentNode!=null)
+            {
+                Node next = currentNode.next;
+                currentNode.next = previous;
+                previous = currentNode;
+                currentNode = next;
+            }
+
+            
+        }
+
+        public Node addToBigening(int data, Node head)
+        {
+            Node currentNode = head;
+            Node newNode = new Node(data);
+            newNode.next = currentNode;
+            head = newNode;
+
+            return head;
+        }
+
+        public void addToTial(int data, Node head)
+        {
+            if (head == null)
+            {
+                Node newHead = new Node(data);
+                
+            }
+            else
+            {
+                Node currentNode = head;
+                Node newNode = new Node(data);
+
+                while (currentNode.next != null)
+                {
+                    currentNode = currentNode.next;
+                }
+
+
+                currentNode.next = newNode;
+
+               
+            }
+
+            
+        }
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Node head = new Node(5);
+            head.addToTial(20, head);
+            head.addToTial(30, head);
+            head.print(head);
+            Console.WriteLine("-------------------");
+            head = head.addToBigening(70, head);
+            head.print(head);
+            Console.WriteLine("-------------------");
+            head.reverseLinkedList(head);
+            
+            Console.ReadKey();
+        }
+    }
+}
